e0703ec6177f9aff62e7505e3675f9a8555f6801
Mallory Milstead <mallorymilstead@gmail.com>
2024-05-01 19:26:53 +0000

    form changes

--- flask_app/flask_app.py
+++ flask_app/flask_app.py
@@ -38,7 +38,7 @@ def survey():
                 #Split the user's form input into a list of words
                 user_string = user_pets.split(" ")
                 print(user_string)
-                return redirect(url_for('results'))
+                return redirect(url_for('results'), display_info = user_string)
 
 
 
